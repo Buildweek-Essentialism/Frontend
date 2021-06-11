23b78282ed98fd626c49dc56bad01e0ef2602199
“nadiachilds” <Nadiachilds@outlook.com>
2021-06-11 00:03:51 -0400

    cleaning up formatting round 1

--- essentialism/src/index.js
+++ essentialism/src/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-import './index.css';
+import "./index.css";
 
-import App from './App';
-import rootReducer from './reducer/reducer';
+import App from "./App";
+import rootReducer from "./redux/reducer/reducer";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-	<Provider store={store}>
-		<App />,
-	</Provider>,
-	rootElement,
+  <Provider store={store}>
+    <App />,
+  </Provider>,
+  rootElement
 );
-
-
